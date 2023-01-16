db97193528e55d43e80ea87a6397248e4bcf13e5
Antonio <netoifpe@gmail.com>
2023-01-16 14:47:58 -0300

    fix: dateProjects array

--- src/scripts/dateProjects.js
+++ src/scripts/dateProjects.js
@@ -1,5 +1,20 @@
 export const infosProjects = [
   {
+    name: "Close Worker",
+    tags: ["REACT", "API", "CSS"],
+    imgCapa: "../../assets/capaCloseWorker.png",
+    linkSite: "https://close-work.vercel.app/home",
+    linkGithub: "https://github.com/AntonioSantosBJPE/closeWorker",
+  },
+  {
+    name: "Hamburgueria Kenzie",
+    tags: ["REACT", "API", "CSS"],
+    imgCapa: "../../assets/capaHamburgueria.png",
+    linkSite:
+      "https://react-entrega-template-hamburgueria-da-kenzie-2-0.vercel.app/login",
+    linkGithub: "https://github.com/AntonioSantosBJPE/hamburgueria",
+  },
+  {
     name: "Kenzie Hub",
     tags: ["REACT", "API", "CSS"],
     imgCapa: "../../assets/capaKenzieHub.png",
@@ -7,21 +22,7 @@ export const infosProjects = [
       "https://react-entrega-kenzie-hub-complete-antonio-santos-bjpe.vercel.app/login",
     linkGithub: "https://github.com/AntonioSantosBJPE/Kenzie-Hub",
   },
-  {
-    name: "Hamburgueria Kenzie",
-    tags: ["REACT", "API", "CSS"],
-    imgCapa: "../../assets/capaBburguerkenzie.png",
-    linkSite:
-      "https://react-entrega-template-hamburgueria-da-kenzie-antoniosantosbjpe.vercel.app/",
-    linkGithub: "https://github.com/AntonioSantosBJPE/Hamburgueria-da-Kenzie",
-  },
-  {
-    name: "Nu kenzie",
-    tags: ["REACT", "CSS"],
-    imgCapa: "../../assets/capaNuKenzie.png",
-    linkSite: "https://nu-kenzie-beta-five.vercel.app/",
-    linkGithub: "https://github.com/AntonioSantosBJPE/NuKenzie",
-  },
+
   {
     name: "RocketFlix",
     tags: ["HTML", "CSS", "JS", "API"],
@@ -43,18 +44,4 @@ export const infosProjects = [
     linkSite: "https://antoniosantosbjpe.github.io/Calculadora/",
     linkGithub: "https://github.com/AntonioSantosBJPE/Calculadora",
   },
-  // {
-  //   name: "SearchCountries",
-  //   tags: ["HTML", "CSS", "JS", "API"],
-  //   imgCapa: "../../assets/capaSearchCountries.png",
-  //   linkSite: "https://antoniosantosbjpe.github.io/SearchCountries/",
-  //   linkGithub: "https://github.com/AntonioSantosBJPE/SearchCountries",
-  // },
-  // {
-  //   name: "Slide de testemunhos",
-  //   tags: ["HTML", "CSS", "JS"],
-  //   imgCapa: "../../assets/capaSlideTestemunhos.png",
-  //   linkSite: "https://antoniosantosbjpe.github.io/SlideTestemunhos/",
-  //   linkGithub: "https://github.com/AntonioSantosBJPE/SlideTestemunhos",
-  // },
 ];
